e2313ae8cc5e0431c1b86508175268d067671d00
Kirill Amirov <admin@MacBook-Air-ADMIN.local>
2024-12-04 20:45:21 +0200

    ignore

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { updateSession } from "./app/utils/supabase/middleware";
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
-
+//
 export const config = {
   matcher: [
     /*
